dba3a4a33eba0ef1716cc25f184ab5eec4f04342
Kirill Soloshenko <smertlove@yandex.ru>
2022-03-13 20:15:51 +0300

    can't remember what I have done here...

--- project/src/utils.c
+++ project/src/utils.c
@@ -2,15 +2,12 @@
 
 size_t timer_from(unsigned char from) {
     size_t counter = 0;
-    for (unsigned char i = from; i >= 0; --i) {
+    for (unsigned char i = from; i != 0; --i) {
         ++counter;
         printf("%c", i);
     }
     return counter;
 }
-
-
-
 
 
 // TODO: Implement `power of` function
