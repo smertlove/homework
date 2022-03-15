e9a22716d3830590374335cea7336b9c7f52b8f8
Kirill Soloshenko <smertlove@yandex.ru>
2022-03-15 19:16:44 +0300

    NOTE comment removed; TODO comment removed

--- project/src/main.c
+++ project/src/main.c
@@ -11,21 +11,6 @@
 #define TST_REC_IMPL    4
 
 
-/* NOTE(stitaevskiy):
- * We use `atoi` function just for simplification and code reducing.
- * This function doesn't report conversation errors.
- * For safety program we recommend using `strtol` and its analogs.
- * (See `man atoi` and `man strtol` for more info).
- *
- * const char str_num[] = "1234";
- * char* end = NULL;
- * int val = (int) strtol(str_num, &end, 0);
- * if (end != '\0') {
- *     //ERROR
- * }
- *
- * */
-
 int main(int argc, const char** argv) {
     if (argc < 3) {
         return ERR_ARGS_COUNT;
@@ -46,7 +31,7 @@ int main(int argc, const char** argv) {
             if (argc == 4) {
                 int base = atoi(data);
                 int pow =  atoi(argv[3]);
-                int res = custom_pow(base, pow);    // TODO(Kirill Soloshenko): Implement me
+                int res = custom_pow(base, pow);
 
                 printf("%i\n", res);
                 break;
