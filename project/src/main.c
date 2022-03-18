fa5f8412fad4b7bba5f238a32571baa51d769feb
Kirill Soloshenko <smertlove@yandex.ru>
2022-03-19 01:46:25 +0300

    even more cleaning done...

--- project/src/main.c
+++ project/src/main.c
@@ -128,13 +128,32 @@ void transactionWrite(FILE *ofPtr, Data transfer) {
 	}
 }
 
-void blackRecord(FILE *ofPTR, FILE  *ofPTR_2 , FILE *blackrecord , Data	 client_data  ,   Data transfer ){
-while(fscanf( 	ofPTR  , "%d%s%s%s%s%lf%lf%lf",  &client_data.Number ,  client_data.Name  , client_data.Surname   , client_data.addres, client_data.TelNumber, &client_data.indebtedness, &client_data.credit_limit, &client_data.cash_payments)   != -1 )  {
-	while (	fscanf(	ofPTR_2 , "%d %lf",  &transfer.Number , &transfer.cash_payments       ) 	  !=  -1 ){
-			if(     client_data.Number     ==   transfer.Number   &&  transfer.cash_payments != 0         ){
-				client_data.credit_limit += transfer.cash_payments;
+void blackRecord(FILE *ofPTR, FILE  *ofPTR_2, FILE *blackrecord, Data client_data, Data transfer) {
+	while(fscanf(ofPTR,
+		"%d%s%s%s%s%lf%lf%lf",
+		&client_data.Number,
+		client_data.Name,
+		client_data.Surname,
+		client_data.addres,
+		client_data.TelNumber,
+		&client_data.indebtedness,
+		&client_data.credit_limit,
+		&client_data.cash_payments) != -1) {
+			while (fscanf(ofPTR_2, "%d %lf", &transfer.Number, &transfer.cash_payments) != -1) {
+				if(client_data.Number == transfer.Number && transfer.cash_payments != 0) {
+					client_data.credit_limit += transfer.cash_payments;
+				}
 			}
+		fprintf(blackrecord,
+				"%-12d%-11s%-11s%-16s%20s%12.2f%12.2f%12.2f\n",
+				client_data.Number,
+				client_data.Name,
+				client_data.Surname,
+				client_data.addres,
+				client_data.TelNumber,
+				client_data.indebtedness,
+				client_data.credit_limit ,
+				client_data.cash_payments);
+		rewind(ofPTR_2);
 		}
-	fprintf(blackrecord   ,"%-12d%-11s%-11s%-16s%20s%12.2f%12.2f%12.2f\n", client_data.Number, client_data.Name, client_data.Surname, client_data.addres, client_data.TelNumber, client_data.indebtedness,	client_data.credit_limit ,  client_data.cash_payments    ) ;
-	rewind(	ofPTR_2 );
-} }
+}
