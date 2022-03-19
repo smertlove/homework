cc90e04264d395c25a03998e3c914fa130a2eff5
Kirill Soloshenko <smertlove@yandex.ru>
2022-03-19 20:12:51 +0300

    is_prime enhanced a lil bit

--- project/include/is_prime.h
+++ project/include/is_prime.h
@@ -1,6 +1,8 @@
 #ifndef PROJECT_INCLUDE_IS_PRIME_H_
 #define PROJECT_INCLUDE_IS_PRIME_H_
 
-int is_prime(int number);
+#include <stdbool.h>
+
+bool is_prime(int number);
 
 #endif  // PROJECT_INCLUDE_IS_PRIME_H_
--- project/src/is_prime.c
+++ project/src/is_prime.c
@@ -1,13 +1,13 @@
 #include "is_prime.h"
 
-#define PRIME       1  // True
-#define NOT_PRIME   0  // False
-
-int is_prime(int number) {
-    for (int i = 2; i <= number - 1; i++) {
+bool is_prime(int number) {
+    if (number == 2) {
+        return true;
+    }
+    for (int i = 3; i <= number - 1; i+=2) {
         if (number % i == 0) {
-            return NOT_PRIME;
+            return false;
         }
     }
-    return number < 2 ? NOT_PRIME : PRIME;  // Because 0 and 1 are not prime
+    return number < 2 ? false : true;  // Because 0 and 1 are not prime
 }
