6b4d1cc51ae9fce3113f608e1cd2ebcd0a77b8dc
Kirill Soloshenko <smertlove@yandex.ru>
2022-03-20 00:49:21 +0300

    everything should be fine...

--- project/src/main.c
+++ project/src/main.c
@@ -4,7 +4,7 @@
 
 #define ERR_ARGS_COUNT          -1
 #define ERR_WRONG_FLG           -2
-#define ERR_WRONG_ARG			-3
+#define ERR_STRTOL_FAIL			-3
 
 #define TST_TIMER_FIX            1
 #define TST_TIMER_IMPL           2
@@ -16,22 +16,35 @@ int main(int argc, const char** argv) {
     if (argc < 3) {
         return ERR_ARGS_COUNT;
     }
+    char* end = NULL;
+    int test_case = strtol(argv[1], &end, 10);
+    if (*end != '\0') {
+        return ERR_STRTOL_FAIL;
+    }
     
-    int test_case = strtol(argv[1], NULL, 10);
     const char* data;
     data = argv[2];
 
     switch (test_case) {
         case TST_TIMER_FIX: {
-            int to = strtol(data, NULL, 10);
+            int to = strtol(data, &end, 10);
+            if (*end != '\0') {
+                return ERR_STRTOL_FAIL;
+            }
             size_t ticks_count = timer_from(to);
             printf("%zu\n", ticks_count);
             break;
         }
         case TST_TIMER_IMPL: {
             if (argc == 4) {
-                int base = strtol(data, NULL, 10);
-                int pow =  strtol(argv[3], NULL, 10);
+                int base = strtol(data, &end, 10);
+                if (*end != '\0') {
+                    return ERR_STRTOL_FAIL;
+                }
+                int pow =  strtol(argv[3], &end, 10);
+                if (*end != '\0') {
+                    return ERR_STRTOL_FAIL;
+                }
                 int res = custom_pow(base, pow);
                 printf("%i\n", res);
                 break;
@@ -40,13 +53,19 @@ int main(int argc, const char** argv) {
             }
         }
         case TST_CUSTOM_POWER_IMPL: {
-            int num = strtol(data, NULL, 10);
+            int num = strtol(data, &end, 10);
+            if (*end != '\0') {
+                return ERR_STRTOL_FAIL;
+            }
             int res = is_prime(num);
             printf("%i", res);
             break;
         }
         case TST_RECURSIVE_PRINT_IMPL: {
-            int num = strtol(data, NULL, 10);
+            int num = strtol(data, &end, 10);
+            if (*end != '\0') {
+                return ERR_STRTOL_FAIL;
+            }
             print_from_n_to_1(num);
             break;
         }
