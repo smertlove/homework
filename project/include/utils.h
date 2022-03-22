be9d2848792fa0650646a1e1bc3b5899f1858a3d
Kirill Soloshenko <smertlove@yandex.ru>
2022-03-22 20:03:38 +0300

    dumb mistake in utils.h dealt with

--- project/include/utils.h
+++ project/include/utils.h
@@ -5,8 +5,8 @@
 
 #define filename "transaction.dat"
 
-void masterWrite(FILE *ofPTR, Data Client),
-void transactionWrite(FILE *ofPTR, Data transfer),
+void masterWrite(FILE *ofPTR, Data Client);
+void transactionWrite(FILE *ofPTR, Data transfer);
 void blackRecord(FILE *ofPTR, FILE *ofPTR_2, FILE *blackrecord, Data client_data, Data transfer);
 
 #endif // ROJECT_INCLUDE_UTILS_H_
