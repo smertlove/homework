3643b25a12332d8666641d974ee97a1454486a9e
Kirill Soloshenko <smertlove@yandex.ru>
2022-03-22 22:11:31 +0300

    i'm ghoul...

--- project/src/main.c
+++ project/src/main.c
@@ -12,11 +12,11 @@ enum Action {
 #define TRANSACTION_FILENAME "transaction.dat"
 
 struct Client_data {
-	int			Number;
-	char		Name[20];
-	char		Surname[20];
-	char		addres[30];
-	char		TelNumber[15];
+	int			account_number;
+	char		name[20];
+	char		surname[20];
+	char		address[30];
+	char		telephone_number[15];
 	double		indebtedness;
 	double		credit_limit;
 	double		cash_payments;
@@ -88,22 +88,22 @@ void masterWrite(FILE *ofPTR, Client_data Client) {
 			"7 Client credit limit: ",
 			"8 Client cash payments: ");
 	while (scanf(	"%d%s%s%s%s%lf%lf%lf",
-					&Client.Number,
-					Client.Name,
-					Client.Surname,
-					Client.addres,
-					Client.TelNumber,
+					&Client.account_number,
+					Client.name,
+					Client.surname,
+					Client.address,
+					Client.telephone_number,
 					&Client.indebtedness,
 					&Client.credit_limit,
 					&Client.cash_payments
 				) != -1) {
 		fprintf(ofPTR,
 				"%-12d%-11s%-11s%-16s%20s%12.2f%12.2f%12.2f\n",
-				Client.Number,
-				Client.Name,
-				Client.Surname,
-				Client.addres,
-				Client.TelNumber,
+				Client.account_number,
+				Client.name,
+				Client.surname,
+				Client.address,
+				Client.telephone_number,
 				Client.indebtedness,
 				Client.credit_limit,
 				Client.cash_payments);
@@ -123,8 +123,8 @@ void transactionWrite(FILE *ofPtr, Client_data transfer) {
 	printf("%s\n%s\n",
 		   "1 Number account: ",
 		   "2 Client cash payments: ");
-	while(scanf("%d %lf", &transfer.Number, &transfer.cash_payments) != -1) {
-		fprintf(TRANSACTION_FILENAME, "%-3d%-6.2f\n", transfer.Number, transfer.cash_payments);
+	while(scanf("%d %lf", &transfer.account_number, &transfer.cash_payments) != -1) {
+		fprintf(TRANSACTION_FILENAME, "%-3d%-6.2f\n", transfer.account_number, transfer.cash_payments);
 		printf("%s\n%s\n",
 				"1 Number account:",
 				"2 Client cash payments: ");
@@ -136,27 +136,27 @@ void blackRecord(FILE *ofPTR, FILE  *ofPTR_2, FILE *blackrecord, Client_data cli
     int ccount = 0;
 	while((fscanf(ofPTR,
 		"%d%s%s%s%s%lf%lf%lf",
-		&client_data.Number,
-		client_data.Name,
-		client_data.Surname,
-		client_data.addres,
-		client_data.TelNumber,
+		&client_data.account_number,
+		client_data.name,
+		client_data.surname,
+		client_data.address,
+		client_data.telephone_number,
 		&client_data.indebtedness,
 		&client_data.credit_limit,
 		&client_data.cash_payments) != -1) && count < 100) {
-			while ((fscanf(ofPTR_2, "%d %lf", &transfer.Number, &transfer.cash_payments) != -1) && ccount < 100) {
-				if(client_data.Number == transfer.Number && transfer.cash_payments != 0) {
+			while ((fscanf(ofPTR_2, "%d %lf", &transfer.account_number, &transfer.cash_payments) != -1) && ccount < 100) {
+				if(client_data.account_number == transfer.account_number && transfer.cash_payments != 0) {
 					client_data.credit_limit += transfer.cash_payments;
                     count++;
 				}
 			}
 		fprintf(blackrecord,
 				"%-12d%-11s%-11s%-16s%20s%12.2f%12.2f%12.2f\n",
-				client_data.Number,
-				client_data.Name,
-				client_data.Surname,
-				client_data.addres,
-				client_data.TelNumber,
+				client_data.account_number,
+				client_data.name,
+				client_data.surname,
+				client_data.address,
+				client_data.telephone_number,
 				client_data.indebtedness,
 				client_data.credit_limit ,
 				client_data.cash_payments);
