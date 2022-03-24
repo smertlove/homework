11b605428740252a9f744df31e803cfa543e141b
Kirill Soloshenko <smertlove@yandex.ru>
2022-03-24 22:47:30 +0300

    something done

--- project/include/utils.h
+++ project/include/utils.h
@@ -4,6 +4,18 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+typedef struct client_t {
+	int			account_number;
+	char		name[20];
+	char		surname[20];
+	char		address[30];
+	char		telephone_number[15];
+	double		indebtedness;
+	double		credit_limit;
+	double		cash_payments;
+} client_t;
+
+
 void masterWrite(FILE *ofPTR, client_t Client);
 void transactionWrite(FILE *ofPTR, client_t transfer);
 void blackRecord(FILE *ofPTR, FILE *ofPTR_2, FILE *blackrecord, client_t client_data, client_t transfer);
--- project/src/main.c
+++ project/src/main.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "utils.h"
 
 enum action {
@@ -11,24 +12,120 @@ enum action {
 #define BLACKRECORD_FILENAME	"blackrecord.dat"
 #define FILE_ACCESS_ERROR		"--- ERROR: NO ACCESS ---"
 
-struct client_t {
-	int			account_number;
-	char		name[20];
-	char		surname[20];
-	char		address[30];
-	char		telephone_number[15];
-	double		indebtedness;
-	double		credit_limit;
-	double		cash_payments;
-};
-typedef struct client_t client_t;
+
+
+
+int get_case_choice(void) {
+	puts("please enter action\n1 enter data client:\n2 enter data transaction:\n3 update base\n");
+	int case_choice;
+	scanf("%d", &case_choice);
+	return case_choice;
+}
+
+void print_client_data_fields(void) {
+	printf("%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n\n",
+			"1 Number account: ",
+			"2 Client name: ",
+			"3 Surname: ",
+			"4 Addres client: ",
+			"5 Client Telnum: ",
+			"6 Client indebtedness: ",
+			"7 Client credit limit: ",
+			"8 Client cash payments: ");
+}
+
+void masterWrite(FILE *ofPTR, client_t Client) {
+	print_client_data_fields();
+	while (scanf(	"%d%s%s%s%s%lf%lf%lf",
+					&Client.account_number,
+					Client.name,
+					Client.surname,
+					Client.address,
+					Client.telephone_number,
+					&Client.indebtedness,
+					&Client.credit_limit,
+					&Client.cash_payments
+				) != -1) {
+		fprintf(ofPTR,
+				"%-12d%-11s%-11s%-16s%20s%12.2f%12.2f%12.2f\n",
+				Client.account_number,
+				Client.name,
+				Client.surname,
+				Client.address,
+				Client.telephone_number,
+				Client.indebtedness,
+				Client.credit_limit,
+				Client.cash_payments);
+		print_client_data_fields();
+		while(getchar() != '\n');
+	}
+}
+
+void transactionWrite(FILE *ofPtr, client_t transfer) {
+	printf("%s\n%s\n",
+		   "1 Number account: ",
+		   "2 Client cash payments: ");
+	while(scanf("%d %lf", &transfer.account_number, &transfer.cash_payments) != -1) {
+		fprintf(ofPtr, "%-3d%-6.2f\n", transfer.account_number, transfer.cash_payments);
+		printf("%s\n%s\n",
+				"1 Number account:",
+				"2 Client cash payments: ");
+	}
+}
+
+void blackRecord(FILE *ofPTR, FILE  *ofPTR_2, FILE *blackrecord, client_t client_data, client_t transfer) {
+	while(fscanf(ofPTR,
+		"%d%s%s%s%s%lf%lf%lf",
+		&client_data.account_number,
+		client_data.name,
+		client_data.surname,
+		client_data.address,
+		client_data.telephone_number,
+		&client_data.indebtedness,
+		&client_data.credit_limit,
+		&client_data.cash_payments) != -1) {
+			while ((fscanf(ofPTR_2, "%d %lf", &transfer.account_number, &transfer.cash_payments) != -1)) {
+				if(client_data.account_number == transfer.account_number && transfer.cash_payments != 0) {
+					client_data.credit_limit += transfer.cash_payments;
+				}
+			}
+		fprintf(blackrecord,
+				"%-12d%-11s%-11s%-16s%20s%12.2f%12.2f%12.2f\n",
+				client_data.account_number,
+				client_data.name,
+				client_data.surname,
+				client_data.address,
+				client_data.telephone_number,
+				client_data.indebtedness,
+				client_data.credit_limit ,
+				client_data.cash_payments);
+		rewind(ofPTR_2);
+		}
+}
+
+
+
+
 
 int main(void){
+	// int count = 0;
+	// int a;
+	// 	int b;
+	// 	char c;
+	// 	int scn;
+	// 		while(1){
+	// 	printf("%s\n", "enter some digits");
+		
+	// 	scn = scanf("%d%d%s", &a, &b, &c);
+
+	// 	while(getchar() != '\n');
+
+	// 	printf("int a = %d\nint b = %d\nchar c = %s\n", a, b, &c);
+	// 	printf("scan_return = %d\n--- COUNT %d ---\n",  scn, count);
+	// 	count++;
+	// }
+	
 	int case_choice = get_case_choice();
-
-	
-	
-
 	while (case_choice != -1) {
 		switch(case_choice) {
 			case ENTER_CLIENT_DATA: {
@@ -80,97 +177,6 @@ int main(void){
 	return 0;
 }
 
-void masterWrite(FILE *ofPTR, client_t Client) {
-	printf("%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n\n",
-			"1 Number account: ",
-			"2 Client name: ",
-			"3 Surname: ",
-			"4 Addres client: ",
-			"5 Client Telnum: ",
-			"6 Client indebtedness: ",
-			"7 Client credit limit: ",
-			"8 Client cash payments: ");
-	while (scanf(	"%d%s%s%s%s%lf%lf%lf",
-					&Client.account_number,
-					Client.name,
-					Client.surname,
-					Client.address,
-					Client.telephone_number,
-					&Client.indebtedness,
-					&Client.credit_limit,
-					&Client.cash_payments
-				) != -1) {
-		fprintf(ofPTR,
-				"%-12d%-11s%-11s%-16s%20s%12.2f%12.2f%12.2f\n",
-				Client.account_number,
-				Client.name,
-				Client.surname,
-				Client.address,
-				Client.telephone_number,
-				Client.indebtedness,
-				Client.credit_limit,
-				Client.cash_payments);
-		printf("%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n\n",
-				"1 Number account: ",
-				"2 Client name: ",
-				"3 Surname: ",
-				"4 Addres client: ",
-				"5 Client Telnum: ",
-				"6 Client indebtedness: ",
-				"7 Client credit limit: ",
-				"8 Client cash payments:");
-	}
-}
 
-void transactionWrite(FILE *ofPtr, client_t transfer) {
-	printf("%s\n%s\n",
-		   "1 Number account: ",
-		   "2 Client cash payments: ");
-	while(scanf("%d %lf", &transfer.account_number, &transfer.cash_payments) != -1) {
-		fprintf(TRANSACTION_FILENAME, "%-3d%-6.2f\n", transfer.account_number, transfer.cash_payments);
-		printf("%s\n%s\n",
-				"1 Number account:",
-				"2 Client cash payments: ");
-	}
-}
 
-void blackRecord(FILE *ofPTR, FILE  *ofPTR_2, FILE *blackrecord, client_t client_data, client_t transfer) {
-    int count = 0;
-    int ccount = 0;
-	while((fscanf(ofPTR,
-		"%d%s%s%s%s%lf%lf%lf",
-		&client_data.account_number,
-		client_data.name,
-		client_data.surname,
-		client_data.address,
-		client_data.telephone_number,
-		&client_data.indebtedness,
-		&client_data.credit_limit,
-		&client_data.cash_payments) != -1) && count < 100) {
-			while ((fscanf(ofPTR_2, "%d %lf", &transfer.account_number, &transfer.cash_payments) != -1) && ccount < 100) {
-				if(client_data.account_number == transfer.account_number && transfer.cash_payments != 0) {
-					client_data.credit_limit += transfer.cash_payments;
-                    count++;
-				}
-			}
-		fprintf(blackrecord,
-				"%-12d%-11s%-11s%-16s%20s%12.2f%12.2f%12.2f\n",
-				client_data.account_number,
-				client_data.name,
-				client_data.surname,
-				client_data.address,
-				client_data.telephone_number,
-				client_data.indebtedness,
-				client_data.credit_limit ,
-				client_data.cash_payments);
-		rewind(ofPTR_2);
-        count++;
-		}
-}
 
-int get_case_choice(void) {
-	printf("%s", "please enter action\n1 enter data client:\n2 enter data transaction:\n3 update base\n");
-	int case_choice;
-	scanf("%d", &case_choice);
-	return case_choice;
-}
