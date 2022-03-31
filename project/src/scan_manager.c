6b07d857293934e4a578c557d4351057cb009f1f
Kirill Soloshenko <smertlove@yandex.ru>
2022-03-31 12:25:02 +0300

    #

--- project/src/scan_manager.c
+++ project/src/scan_manager.c
@@ -20,33 +20,33 @@ int scan_client_data(client_t *client) {
 }
 
 int scan_transaction_data(client_t *client) {
-	int argc = scanf(TRANSACTION_SCAN_PATTERN,
-	&client->account_number,
-	&client->cash_payments
+	int argc = scanf(	TRANSACTION_SCAN_PATTERN,
+						&client->account_number,
+						&client->cash_payments
 	);
 	return argc;
 }
 
 int read_client_data(FILE *file, client_t *client){
-    int argc = fscanf(file,
-	CLIENT_DATA_SCAN_PATTERN,
-	&client->account_number,
-	client->name,
-	client->surname,
-	client->address,
-	client->telephone_number,
-	&client->indebtedness,
-	&client->credit_limit,
-	&client->cash_payments
+    int argc = fscanf(	file,
+						CLIENT_DATA_SCAN_PATTERN,
+						&client->account_number,
+						client->name,
+						client->surname,
+						client->address,
+						client->telephone_number,
+						&client->indebtedness,
+						&client->credit_limit,
+						&client->cash_payments
 	);
     return argc;
 }
 
 int read_transaction_data(FILE *file, client_t *transfer) {
-    int argc = fscanf(file,
-	TRANSACTION_SCAN_PATTERN,
-	&transfer->account_number,
-	&transfer->cash_payments
+    int argc = fscanf(	file,
+						TRANSACTION_SCAN_PATTERN,
+						&transfer->account_number,
+						&transfer->cash_payments
 	);
     return argc;
 }
