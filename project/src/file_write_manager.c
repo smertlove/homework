f53cec6448438a2a70002130f36c768ce4b6f625
Kirill Soloshenko <smertlove@yandex.ru>
2022-03-31 12:23:51 +0300

    #

--- project/src/file_write_manager.c
+++ project/src/file_write_manager.c
@@ -7,22 +7,23 @@
 
 void write_client_data(FILE *file, client_t client) {
 	fprintf(file,
-			CLIENT_DATA_WRITE_PATTERN,
-			client.account_number,
-			client.name,
-			client.surname,
-			client.address,
-			client.telephone_number,
-			client.indebtedness,
-			client.credit_limit,
-			client.cash_payments);
+	CLIENT_DATA_WRITE_PATTERN,
+	client.account_number,
+	client.name,
+	client.surname,
+	client.address,
+	client.telephone_number,
+	client.indebtedness,
+	client.credit_limit,
+	client.cash_payments
+	);
 }
 
 void write_transaction_data(FILE *file, client_t transaction) {
 	fprintf(
-		file,
-		TRANSACTION_WRITE_PATTERN,
-		transaction.account_number,
-		transaction.cash_payments
+	file,
+	TRANSACTION_WRITE_PATTERN,
+	transaction.account_number,
+	transaction.cash_payments
 	);
 }
