f088f5de7ffb1e8504c58373196dd99b154ef18b
Kirill Soloshenko <smertlove@yandex.ru>
2022-04-24 17:27:55 +0300

    ##

--- run_linters.sh
+++ run_linters.sh
@@ -27,7 +27,7 @@ check_log "cppcheck project --enable=all --inconclusive --error-exitcode=1 -I pr
 print_header "RUN clang-tidy"
 check_log "clang-tidy project/src/* project/include/* -warnings-as-errors=* -extra-arg=-std=c99 -- -Iproject/include" "Error (?:reading|while processing)"
 
-# print_header "RUN cpplint"
-# check_log "cpplint --extensions=c project/include/* project/src/*" "Can't open for reading"
+print_header "RUN cpplint"
+check_log "cpplint --extensions=c project/include/* project/src/*" "Can't open for reading"
 
 print_header "SUCCESS"
