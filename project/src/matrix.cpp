5dd9c034de9af1c12f49099ed65f1d13affa6540
Kirill Soloshenko <smertlove@yandex.ru>
2022-04-26 18:24:53 +0300

    now builds successfully

--- project/src/matrix.cpp
+++ project/src/matrix.cpp
@@ -25,29 +25,29 @@ Matrix::Matrix(std::istream& is) {
     }
 }
 
-Matrix::getRows() {
+size_t Matrix::getRows() const {
     return row_count;
 }
 
-Matrix::getCols() {
+size_t Matrix::getCols() const {
     return col_count;
 }
 
-Matrix::operator()(size_t i, size_t j) {
-    return &data[i * col_count + j];
-}
-
-Matrix::operator()(size_t i, size_t j) const {
+double Matrix::operator()(size_t i, size_t j) const {
     return data[i * col_count + j];
 }
 
-Matrix::operator==(const Matrix& rhs) const {
-    if (this->row_count != rhs::getRows() || this->col_count != rhs::getCols()) {
+double& Matrix::operator()(size_t i, size_t j) {
+    return data[i * col_count + j];
+}
+
+bool Matrix::operator==(const Matrix& rhs) const {
+    if (row_count != rhs.getRows() || col_count != rhs.getCols()) {
         return false;
     }
     for (size_t i = 0; i < row_count; i++) {
-        for (size_t i = 0; i < col_count; i++) {
-            if (this(i, j) != rhs(i, j)) {
+        for (size_t j = 0; j < col_count; i++) {
+            if (operator()(i, j) != rhs(i, j)) {
                 return false;
             }
         }
@@ -55,37 +55,37 @@ Matrix::operator==(const Matrix& rhs) const {
     return true;
 }
 
-Matrix::operator!=(const Matrix& rhs) {
-    return !(this->operator==(rhs));
+bool Matrix::operator!=(const Matrix& rhs) const {
+    return !(operator==(rhs));
 }
 
-Matrix::operator+(const Matrix& rhs) const {
-    Matrix new_matrix = new Matrix(row_count, col_count);
+Matrix Matrix::operator+(const Matrix& rhs) const {
+    Matrix new_matrix = Matrix(row_count, col_count);
     for (size_t i = 0; i < row_count; i++) {
         for (size_t j = 0; j < col_count; j++) {
-            new_matrix(i, j) = this(i, j) + rhs(i, j);
+            new_matrix(i, j) = operator()(i, j) + rhs(i, j);
         }
     }
     return new_matrix;
 }
 
-Matrix::operator-(const Matrix& rhs) const {
-    Matrix new_matrix = new Matrix(row_count, col_count);
+Matrix Matrix::operator-(const Matrix& rhs) const {
+    Matrix new_matrix = Matrix(row_count, col_count);
     for (size_t i = 0; i < row_count; i++) {
         for (size_t j = 0; j < col_count; j++) {
-            new_matrix(i, j) = this(i, j) - rhs(i, j);
+            new_matrix(i, j) = operator()(i, j) - rhs(i, j);
         }
     }
     return new_matrix;
 }
 
-Matrix::operator*(const Matrix& rhs) const {
-    Matrix new_matrix = new Matrix(row_count, rhs::getCols());
+Matrix Matrix::operator*(const Matrix& rhs) const {
+    Matrix new_matrix = Matrix(row_count, rhs.getCols());
     for (size_t i = 0; i < row_count; i++) {
-        for (size_t j = 0; j < rhs::getCols(); j++) {
+        for (size_t j = 0; j < rhs.getCols(); j++) {
             double total = 0.0;
             for (size_t k = 0; k < col_count; k++) {
-                total = total + this(i, k) * rhs(k, j);
+                total = total + operator()(i, k) * rhs(k, j);
             }
             new_matrix(i, j) = total;
         }
@@ -93,33 +93,45 @@ Matrix::operator*(const Matrix& rhs) const {
     return new_matrix;
 }
 
-Matrix::operator*(double val) const {
-    Matrix new_matrix = new Matrix(row_count, rhs::getCols());
+Matrix Matrix::operator*(double val) const {
+    Matrix new_matrix = Matrix(row_count, col_count);
     for (size_t i = 0; i < row_count; i++) {
         for (size_t j = 0; j < col_count; j++) {
-            new_matrix(i, j) = this(i, j) * val;
+            new_matrix(i, j) = operator()(i, j) * val;
         }
     }
+    return new_matrix;
 }
 
 Matrix operator*(double val, const Matrix& matrix) {
     return matrix * val;
 }
 
-Matrix::transp() const {
-    return this;
+std::ostream& operator<<(std::ostream& os, const Matrix& matrix) {
+    for (size_t i = 0; i < matrix.getRows(); i++) {
+        for (size_t j = 0; j < matrix.getCols(); j++) {
+            os << matrix(i, j);
+        }
+        os << std::endl;
+    }
+    return os;
 }
 
-Matrix::det() const {
-    return this;
+
+Matrix Matrix::transp() const {
+    return Matrix(row_count, col_count);
 }
 
-Matrix::adj() const {
-    return this;
+double Matrix::det() const {
+    return 10.0;
 }
 
-Matrix inv() {
-    return this;
+Matrix Matrix::adj() const {
+    return Matrix(row_count, col_count);
+}
+
+Matrix Matrix::inv() const {
+    return Matrix(row_count, col_count);
 }
 
 }  // namespace prep
