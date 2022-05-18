7f4031d1ddf618bf8ad25720612f70121602e64f
Kirill Soloshenko <smertlove@yandex.ru>
2022-05-19 01:33:02 +0300

    100-7

--- src/list.hpp
+++ src/list.hpp
@@ -1,6 +1,6 @@
-#pragma once  // NOLINT
 #include <iterator>
 
+// wsl gcc test.hpp -std=c++17 -g -fdiagnostics-color -O0 -Wall -Wextra -Werror -I. 
 
 namespace task {
 
@@ -54,8 +54,8 @@ public:
             current = current->next;
             return old;
         }
-        reference operator*() const { return &current; }
-        pointer operator->() const { return *current; }
+        reference operator*() const { return current; }
+        pointer operator->() const { return current; }
         iterator& operator--() {
             current = current->prev;
             return *this;
@@ -66,7 +66,7 @@ public:
             return old;
         }
 
-        bool operator==(iterator other) const { return current == other->current ? true : false; }
+        bool operator==(iterator other) const { return current == other->current}
         bool operator!=(iterator other) const { return !((*this) == other); }
 
     };
@@ -95,8 +95,8 @@ public:
             current = current->next;
             return old;
         }
-        reference operator*() const { return &current; }
-        pointer operator->() const { return *current; }
+        reference operator*() const { return current; }
+        pointer operator->() const { return current; }
         const_iterator& operator--() {
             current = current->prev;
             return *this;
@@ -107,13 +107,12 @@ public:
             return old;
         }
 
-        bool operator==(const_iterator other) const { return current == other->current ? true : false; }
-        bool operator!=(const_iterator other) const { return !((*this) == other); }
+        bool operator==(const_iterator other) const { return current == other->current}
+        bool operator!=(const_iterator other) const { return (*this) != other; }
     };
 
     using reverse_iterator = std::reverse_iterator<iterator>;
     using const_reverse_iterator = std::reverse_iterator<const_iterator>;
-
 
     list();
     list(size_t count, const T& value);
@@ -171,16 +170,11 @@ public:
     void reverse();
     void unique();
     void sort();
-
-    // Your code goes here?..
-
 };
-
-/*********** list ***********/
 
 template<class T>
 list<T>::list() {
-    size = 0;
+    length = 0;
     head = nullptr;
     tail = nullptr;
 }
@@ -200,8 +194,26 @@ list<T>::list(size_t count) {
 
 template<class T>
 list<T>::~list() {
-    while (head != nullptr) {
-        pop_back();
+    clear();
+}
+
+template<class T>
+void list<T>::push_back(const T& value) {
+    if (head == nullptr) {
+        head = new list_node<T>(value);
+        tail = head;
+    } else {
+        tail->next = new list_node<T>(value, nullptr, tail);
+        tail = tail->next;
+    }
+}
+
+template<class T>
+void list<T>::pop_back() {
+    if (tail == nullptr) {
+        return;
+    } else {
+        tail = tail->prev;
     }
 }
 
@@ -228,28 +240,28 @@ template<class T>
 const T& list<T>::back() const { return tail; };
 
 template<class T>
-list<T>::iterator list<T>::begin() const { return iterator(head); }
+typename list<T>::iterator list<T>::begin() const { return iterator(head); }
 
 template<class T>
-list<T>::iterator list<T>::end() const {return iterator(tail);}
+typename list<T>::iterator list<T>::end() const {return iterator(tail);}
 
 template<class T>
-list<T>::const_iterator list<T>::cbegin() const { return const_iterator(head); }
+typename list<T>::const_iterator list<T>::cbegin() const { return const_iterator(head); }
 
 template<class T>
-list<T>::const_iterator list<T>::cend() const { return const_iterator(end); }
+typename list<T>::const_iterator list<T>::cend() const { return const_iterator(end); }
 
 template<class T>
-list<T>::reverse_iterator list<T>::rbegin() const { return reverse_iterator(head); }
+typename list<T>::reverse_iterator list<T>::rbegin() const { return reverse_iterator(head); }
 
 template<class T>
-list<T>::reverse_iterator list<T>::rend() const { return reverse_iterator(end); }
+typename list<T>::reverse_iterator list<T>::rend() const { return reverse_iterator(end); }
 
 template<class T>
-list<T>::const_reverse_iterator list<T>::crbegin() const { return const_reverse_iterator(head); }
+typename list<T>::const_reverse_iterator list<T>::crbegin() const { return const_reverse_iterator(head); }
 
 template<class T>
-list<T>::const_reverse_iterator list<T>::crend() const { return const_reverse_iterator(end); }
+typename list<T>::const_reverse_iterator list<T>::crend() const { return const_reverse_iterator(end); }
 
 template<class T>
 bool list<T>::empty() const { return length == 0; }
@@ -261,50 +273,20 @@ template<class T>
 size_t list<T>::max_size() const { return 1000000; };
 
 template<class T>
-void list<T>::clear();
-
-template<class T>
-list<T>::iterator list<T>::insert(const_iterator pos, const T& value) {
-    list_node *new_node = new list_node(value);
-    pos->current
-}
-
-template<class T>
-list<T>::iterator list<T>::insert(const_iterator pos, size_t count, const T& value);
-
-template<class T>
-list<T>::iterator list<T>::erase(const_iterator pos);
-
-template<class T>
-list<T>::iterator list<T>::erase(const_iterator first, const_iterator last);
-
-template<class T>
-void list<T>::push_back(const T& value) {
-    if (head == nullptr) {
-        head = new list_node(data);
-        tail = head;
-    } else {
-        tail->next = new list_node(data, nullptr, tail);
-        tail = tail->next;
+void list<T>::clear() {
+    while (head != nullptr) {
+        pop_back();
     }
-}
-
-template<class T>
-void list<T>::pop_back() {
-    if (tail == nullptr) {
-        return;
-    } else {
-        tail = tail->prev;
-    }
+    length = 0;
 }
 
 template<class T>
 void list<T>::push_front(const T& value) {
     if (head == nullptr) {
-        head = new list_node(data);
+        head = new list_node<T>(value);
         tail = head;
     } else {
-        head->prev = new list_node(data, head);
+        head->prev = new list_node<T>(value, head);
         head = head->prev;
     }
 }
@@ -319,9 +301,57 @@ void list<T>::pop_front() {
 }
 
 template<class T>
-void list<T>::resize(size_t count){return;};
+typename list<T>::iterator list<T>::insert(const_iterator pos, const T& value) {
+    const_iterator pos_next = pos + 1;
+    list_node<T> new_node = new list_node<T>(value);
+    pos.current->next = new_node;
+    pos_next.current->prev = new_node;
+    pos++;
+    return pos;
+}
+
 template<class T>
-void list<T>::swap(list<T>& other){return;};
+typename list<T>::iterator list<T>::insert(const_iterator pos, size_t count, const T& value) {
+    const_iterator pos_next = pos + 1;
+    for (size_t i = 0; i < count; i++) {
+        pos = insert(pos, value);
+    }
+    return pos;
+}
+
+template<class T>
+typename list<T>::iterator list<T>::erase(const_iterator pos) {
+    pos.current->prev->next = pos.current->next;
+    pos.current->next->prev = pos.current->prev;
+    pos--;
+    return pos;
+}
+
+template<class T>
+typename list<T>::iterator list<T>::erase(const_iterator first, const_iterator last) {
+    first.current->prev->next = last.current;
+    last.current->prev = first.current->prev;
+    first--;
+    return first;
+}
+
+template<class T>
+void list<T>::resize(size_t count) {
+    while (length != count) {
+        if (length > count) {
+            pop_back();
+        } else {
+            push_back(T());
+        }
+    }
+}
+
+template<class T>
+void list<T>::swap(list& other) {
+    list<T> buf = *this;
+    this = other;
+    other = buf;
+}
 
 template<class T>
 void list<T>::merge(list<T>& other) {return;};
@@ -337,65 +367,4 @@ void list<T>::unique(){return;};
 template<class T>
 void list<T>::sort(){return;};
 
-
-
 }  // namespace task
-
-
-
-
-
-
-
-// list<T>::push_front(const T& value) {
-//     if (head == nullptr) {
-//         head = new list_node(data);
-//         tail = head;
-//     } else {
-//         head->prev = new list_node(data, head);
-//         head = head->prev;
-//     }
-// }
-
-// list<T>::push_front() {
-//     push_front(T());
-// }
-
-// T& list<T>::front() {
-//     return *head;
-// }
-
-
-
-// const T& front() const {
-//     return *head;
-// }
-
-// T& back() {
-//     return *tail;
-// }
-
-// const T& back() const {
-//     return *tail;
-// }
-
-// list<T>::list(size_t count, const T& value) {
-//     list();
-//     size = count;
-//     for (size_t i = 0; i < count; i++) {
-//         push_back(value);
-//     }
-// }
-// list<T>::list(size_t count) {
-//     list(count, T())
-// }
-// list<T>::~list() = default;
-
-// list<T>::list(const list& other) {
-//     this->size = other.size;
-//     this->head = other.head;
-//     this->tail = other.tail;
-// }
-// list& operator=(const list& other) {
-//     return list<T>(other);
-// }
